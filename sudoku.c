90e8b93151479116d75bdd485e43e4f9d6aa1e7a
Andrew Clough <andrew.clough@gmail.com>
2012-05-30 19:21:38 -0400

    Further progress on the C sudokuer.  Got loading and printing working. Redid the constants describing the size of the puzzle.

--- sudoku.c
+++ sudoku.c
@@ -4,18 +4,82 @@
 int main(int argc, char *argv[]){
     if(argc < 2){
         printf("Please specify a sudoku to solve\n");
-        return 0;
+        return -1;
     }
     sudoku *puzzle = (sudoku*)malloc(sizeof(sudoku));
-    load_sudoku( puzzle, argv[1]);
+    if(load_sudoku( puzzle, argv[1])){
+        return -1;
+    }
+    print_sudoku(puzzle);
+    if(solve_sudoku(puzzle)){
+        return -1;
+    }
+    print_sudoku(puzzle);
     return 0;
 }
 
 int load_sudoku(sudoku *puzzle, char *filename){
     FILE *fp;
+    int value;
     printf("Loading file: %s\n", filename);
     fp = fopen(filename, "r");
-
+    for(int i = 0; i < SUDOKU_SIZE; i++){
+        if(fscanf(fp, "%d", &value)){
+            puzzle->value[i] = 1 << (value -1); // It's stored as a bit field
+        } else {
+            printf("Invalid input or not enough values\n");
+            return -1;
+        }
+    }
+    if(
     printf("Finished\n");
     return 0;
 }
+
+int solve_sudoku(sudoku *puzzle){
+    int old_solved = SUDOKU_SIZE;
+    int new_solved = do_sudoku(puzzle);
+    while(new_solved){
+        if(old_solved == new_solved){
+            printf("Could not solve sudoku\n");
+            return old_solved;
+        } 
+        old_solved = new_solved;
+        new_solved = do_sudoku(puzzle);
+    }
+    printf("solved sudoku!\n");
+    return 0;
+}
+
+void print_sudoku(sudoku *puzzle){
+    int x;
+    printf("\n");
+    for(int i = 0; i < SUDOKU_SIZE; i++){
+        x = field_to_int(puzzle->value[i]);
+        printf("%2d ", x);
+        if( 0 == (i+1) % SUDOKU_BOX_LENGTH){
+            printf(" ");
+        }
+        if( 0 == (i+1) % SUDOKU_LENGTH){
+            printf("\n");
+        }
+        if( 0 == (i+1) % (SUDOKU_LENGTH * SUDOKU_BOX_LENGTH)){
+            printf("\n");
+        }
+    }
+    printf("\n");
+}
+
+int field_to_int(int x){
+    int y = 1;
+    while(x > 0){
+        if(1 == x){
+            return y;
+        } else {
+            y++;
+            x >>= 1;
+        }
+    }
+    return 0;
+}
+
--- sudoku.h
+++ sudoku.h
@@ -1,18 +1,24 @@
-#define SUDOKU_SIZE 9   //This is the number of possible different numbers, and
+#define SUDOKU_BOX_LENGTH 3 // The square root of the SUDOKU_SIZE
+#define SUDOKU_LENGTH (SUDOKU_BOX_LENGTH*SUDOKU_BOX_LENGTH) 
+                        // This is the number of possible different numbers, and
                         // thus both the length and width of the grid, and the
                         // number of items in a box
                         // Must be a sqaure for everything to work out
-#define SUDOKU_BOX_SIZE 3 // The square root of the SUDOKU_SIZE
+#define SUDOKU_SIZE (SUDOKU_LENGTH * SUDOKU_LENGTH)
+                        // The total number of elements in the SUDOKU
+
 
 typedef struct {
-    int value[SUDOKU_SIZE][SUDOKU_SIZE];   // A bit field that says what the 
-                                           // Final value of that space is.
-                                           // Only 1 bit should be set!
-    int possibles[SUDOKU_SIZE][SUDOKU_SIZE]; // Encodes a bit field, where bit X
-                                             // says whether the value at that
-                                             // location could be X
+    int value[SUDOKU_SIZE];   // A bit field that says what the 
+                              // Final value of that space is.
+                              // Only 1 bit should be set!
+    int possibles[SUDOKU_SIZE]; // Encodes a bit field, where bit X
+                                // says whether the value at that
+                                // location could be X
 } sudoku;
 
-
 int load_sudoku( sudoku*, char*);
+int solve_sudoku( sudoku*);
+int do_sudoku( sudoku*);
 void print_sudoku( sudoku*);
+int field_to_int(int);

ed1a98abca24f8f8149b534b63dbd02a2bc38c4e
Andrew Clough <aclough@arctus.(none)>
2012-12-14 23:26:29 -0500

    Python version update.
    Changed the python implementation to use generators, and to
    solve all sodukos instead of just easy ones.

--- sudoku.py
+++ sudoku.py
@@ -1,53 +1,58 @@
 import sys
 
-possibilities = {1, 2, 3, 4, 5, 6, 7, 8, 9}
-BLOCK_SIZE = len(possibilities)
-NUM_ROWS = len(possibilities)
-NUM_COLS = len(possibilities)
-SUDOKU_SIZE = NUM_ROWS * NUM_COLS
-NUM_BLOCKS = SUDOKU_SIZE // BLOCK_SIZE
+numbers = {1,2,3,4,5,6,7,8,9}
 debug = False
 
-class Space:
-    """Data structure representing a sudoku space."""
-    value = 0
-    possible = set(possibilities)
-
-class SudokuGrid:
+class Puzzle:
     """Represents a sudoku grid.
 
     It includes methods for loading, solving, and displaying."""
-    _spaces = []
-    _rows = []
-    _colums = []
-    _boxes = []
-    _checks = []
-    _check_flag = False
-    def __init__(self):
-        for i in range(SUDOKU_SIZE):
-            self._spaces.append(Space())
-            self._checks.append(Space())
-        for i in range(NUM_ROWS):
-            self._rows.append(self._spaces[i*NUM_COLS:(i+1)*NUM_COLS])
-        for i in range(NUM_COLS):
-            self._colums.append( self._spaces[i::NUM_ROWS] )
-        for i in range(NUM_BLOCKS):
-            row_divider = NUM_BLOCKS * 3 // NUM_COLS
-            first_row   = (i // row_divider) * 3
-            col_mod = NUM_BLOCKS * 3 // NUM_ROWS
-            first_colum = (i % col_mod) * 3
-            self._boxes.append( [self._rows[x][y] 
-                    for x in range(first_row, first_row + 3) 
-                    for y in range(first_colum, first_colum + 3) ] )
+    _spaces = [] # All the spaces in the puzzle [Space]
+    _possibles = []
+    _solution = []
+    def __init__(self, origonal=None):
+        if debug: print("Starting init")
+        if origonal:
+            self._unsolved = origonal._unsolved
+            self._spaces = []
+            self._possibles = []
+            if origonal._solution:
+                self._solution = origonal._solution
+            # Can't just copy the lists, we need to mutate the new copy.
+            for space in origonal._spaces:
+                self._spaces.append(space)
+            for possible in origonal._possibles:
+                self._possibles.append(set(possible))
+        if debug: print("Finishing init")
+
+    def get_row_iter(self, x):
+        for i in range(9):
+            index = 9*x + i
+            yield self._spaces[index], self._possibles[index]
+    def get_col_iter(self, x):
+        for i in range(9):
+            index = x + 9*i
+            yield self._spaces[index], self._possibles[index]
+    def get_blk_iter(self, x):
+        block_index = 3*(x%3) + 27*(x//3)
+        for i in range(9):
+            index = block_index + (i%3) + 9*(i//3)
+            yield self._spaces[index], self._possibles[index]
+
     def load(self, filename):
         """Load a file as the puzzle."""
+        self._spaces = []
+        self._possibles = []
+        self._unsolved = 0
         self._load_helper(filename, self._spaces)
-    def load_check(self, filename):
+        for space in self._spaces:
+            self._possibles.append( set() if space else set(numbers) )
+            if not space: self._unsolved += 1
+    def load_solution(self, filename):
         """Load a file as the solution."""
-        self._load_helper(filename, self._checks)
-        self._check_flag = True
+        self._solution = []
+        self._load_helper(filename, self._solution)
     def _load_helper(self, filename, destination):
-        index = 0
         with open(filename, 'r') as data:
             for line in data:
                 if line.startswith('#'):
@@ -55,115 +60,87 @@ class SudokuGrid:
                 elements = line.split()
                 for element in elements:
                     try:
-                        destination[index].value = int(element)
-                        index += 1
+                        destination.append( int(element))
                     except ValueError:
-                        print("Invalid number \"" + element + "\"")
-        if index != SUDOKU_SIZE:
-            raise ValueError("File contained " + str(index) + " elements!\n" +
-                    str(SUDOKU_SIZE) + " required")
+                        print('Invalid number "' + element + '"')
+        if len(destination) is not len(numbers)**2:
+            raise ValueError("File contained " + str(index) + " elements!")
+
     def print(self):
         """Print out the state of the sudoku."""
-        for row in self._rows:
-            line = ""
-            for space in row:
-                line += str(space.value) + " "
-            print(line)
+        for x in range(9):
+            print([ str(num) + " " for num,pos in self.get_row_iter(x)])
         print("")
+
     def solve(self):
-        """Does a round of solving a sudoku
+        """Solves the sudoku
 
-        This looks both for the only placea a number can go, and the
-        only number that can fit in a space.
-        You'll need to call this multiple times to actually finish solving.
-        It returns the number of blank spaces remaining."""
-        if debug: print("***Rows***")
-        for row in self._rows: 
-            self._reduce_block(row)
-            self._fill_block(row)
-        if debug: print("***Colums***")
-        for colum in self._colums:
-            self._reduce_block(colum)
-            self._fill_block(colum)
-        if debug: print("***Boxes***")
-        for box in self._boxes:
-            self._reduce_block(box)
-            self._fill_block(box)
-        #return number of unsolved elements
-        return len([element for row in self._rows
-                            for element in row 
-                            if element.value == 0])
-    def _reduce_block(self, block):
-        """Takes a block and reduces the possibles to match the values
-
-        Takes a list of Space objects
-        It also fills in any values when there is only one associated
-        possibilty for an element"""
-        if debug: print("*Reducing*")
-        values = set([element.value for element in block])
-        for element in block:
-            element.possible = element.possible.difference(values)
-            if element.value != 0:
-                element.possible = set()
+        First tries to do as much solving as possible without guessing,
+        then finds the space with the fewest possibilities, creates a new
+        Puzzle there with a guess at that space, calls solve() on that puzzle
+        and if it doens't work guesses again"""
+        while True:
+            for i in range(9):
+                self._reduce(self.get_row_iter(i))
+                self._reduce(self.get_col_iter(i))
+                self._reduce(self.get_blk_iter(i))
+            for i in range(len(self._spaces)):
+                if len( self._possibles[i]) is 1:
+                    self._spaces[i] = self.possibles[i].pop()
+            unsolved, index = self.check()
+            if unsolved < self._unsolved:
+                if debug: print(unsolved)
+                self._unsolved = unsolved
+                if unsolved == 0:
+                    self.print()
+                    return self._spaces
+            else:
+                break
+        if debug: print("Entering")
+        for num in self._possibles[index]:
+            new_puzzle = Puzzle(self)
+            new_puzzle._spaces[index] = num
+            try:
+                self._spaces = new_puzzle.solve()
+                return self._spaces
+            except ValueError:
                 continue
-            elif len(element.possible) == 1:
-                element.value = element.possible.pop()
-                if debug: print("!Element now", element.value)
-            elif len(element.possible) == 0 and element.value == 0:
-                self.print()
-                raise Exception("Sudoku not solvable")
-        if self._check_flag:
-           self.do_check()
-    def _fill_block(self, block):
-        """Finds places where a number can only go in one place, and inserts it
+            if debug: print("Exiting")
+            raise ValueError("No solution found")
+    
+    def check(self):
+        """Performs pre-recursion checks
 
-        Operates on a list of Space objects
-        The list must be passed in already reduced"""
-        if debug: print("*Filling*")
-        for i in possibilities:
-            possibles = [element for element in block 
-                    if (i in element.possible and element.value == 0)]
-            if len(possibles) == 1:
-                if possibles[0].value != 0:
-                    raise Exception("Overwrote value in fill " +
-                            str(i) + " over " + str(possibles[0].value))
-                    possibles[0].value = i
-                    possibles[0].possible = set()
-                    if debug: print("!Element now", i)
+        Checks to see how much remains to solve, what index we should guess
+        at to recurse (if neccesary), and checks against the known solution
+        if one is present"""
+        min = 999; min_index = 0; unsolved = 0
+        for i in range(len(self._spaces)):
+            num = self._spaces[i]
+            if len(self._possibles[i]) < min and not self._spaces[i]:
+                min_index = i
+            if num == 0:
+                unsolved += 1
+            if self._solution:
+                if num is not self._solution[i] and num is not 0:
+                    if debug: print("Exiting2")
+                    raise ValueError("Sudoku not consistant with solution")
+        if min is 0:
+            if debug: print("Exiting3")
+            raise ValueError("Sudoku cannot be solved due to overrestriction")
+        return unsolved, min_index
 
-        if self._check_flag:
-            self.do_check()
-    def do_check(self):
-       """Checks to solution being made
-
-       Compares the current progress on the solution to an official solution
-       provided, element by element"""
-       for index in range(SUDOKU_SIZE):
-           if self._checks[index].value == 0:
-               continue
-           if self._spaces[index].value == 0:
-               continue
-           if self._checks[index].value != self._spaces[index].value:
-               raise Exception("Generated table does not match solution at " 
-                       + str(index))
-
+    def _reduce(self, local_iter):
+       value_set = set(i[0] for i in local_iter)
+       for num,pos in local_iter:
+           pos.difference_update(value_set)
 
 
 if __name__=='__main__':
-    grid = SudokuGrid()
-    grid.load(sys.argv[1])
-    grid.print()
+    puzzle = Puzzle()
+    puzzle.load(sys.argv[1])
+    puzzle.print()
     if(len(sys.argv) > 2):
-        grid.load_check(sys.argv[2])
-    grid.do_check()
-    unsolved = grid.solve()
-    old_unsolved = unsolved
-    while(unsolved != 0):
-        unsolved = grid.solve()
-        print(unsolved, "remaining")
-        if old_unsolved == unsolved:
-            print("Can't solve")
-            break
-        else:
-            old_unsolved = unsolved
-    grid.print()
+        puzzle.load_solution(sys.argv[2])
+    puzzle.solve()
+    puzzle.print()

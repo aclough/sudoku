f464f273011ba3dd0ea4348b906a0c9dd58f665a
Andrew Clough <andrew.clough@gmail.com>
2019-12-26 21:45:02 -0500

    Actually check in -fPIC for building the library

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 clang -O2 tester.c sudoku.c
-#clang -O2 -shared -o sudoku.so sudoku.c
+clang -O2 -shared -o sudoku.so -fPIC sudoku.c
 nim c -d:release sudoku.nim
 echo "C speed 1000 times"
 time for i in {1..1000}; do ./a.out hard-sudoku.txt > /dev/null; done

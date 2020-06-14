f9928071e6067e284b4e5bd4813435f8dfc8cc5f
Andrew <andrew.clough@gmail.com>
2020-06-14 11:17:30 -0400

    Better printing in test

--- test.sh
+++ test.sh
@@ -1,15 +1,27 @@
 #!/bin/bash -e
+
+echo ""
+echo "Compiling C"
 clang -O2 tester.c sudoku.c
+echo ""
+echo "Compiling C shared library"
 clang -O2 -shared -o sudoku.so -fPIC sudoku.c
-nim c -d:release sudoku.nim
+echo ""
+echo "Compiling Nim"
+nim c -d:release --verbosity:0 sudoku.nim
+
+echo ""
 echo "C speed 1000 times"
 time for i in {1..1000}; do ./a.out hard-sudoku.txt > /dev/null; done
 
+echo ""
 echo "Nim speed 1000 times"
 time for i in {1..1000}; do ./sudoku hard-sudoku.txt > /dev/null; done
 
+echo ""
 echo "Python speed 1000 times"
 time for i in {1..1000}; do python3 sudoku.py hard-sudoku.txt > /dev/null; done
 
+echo ""
 echo "Python calling c speed 1000 times"
 time for i in {1..1000}; do python3 sudoku-with-c.py hard-sudoku.txt > /dev/null; done

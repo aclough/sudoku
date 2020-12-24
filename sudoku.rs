9f4fc03a23ec3baa3ae7f8d9f139183b4cf91a60
Andrew Clough <andrew.clough@gmail.com>
2020-12-23 20:24:38 -0500

    Actually load the puzzle in the rust version
    Puzzle file name is still hardcoded to be 'puzzle.txt'
    
    Needs better error checking on loading the data.

--- sudoku.rs
+++ sudoku.rs
@@ -1,7 +1,8 @@
 // Rust sudoku implemenation
 // This is a heuristic guided search with backout like the Nim version
-// There's some threading here, just to explore it
+// I'll be trying to add threading later
 
+use std::fs;
 
 struct Sudoku {
     vals: [u8; 81],
@@ -12,7 +13,6 @@ impl Sudoku {
         let mut s = "".to_string();
         for (i, v) in self.into_iter().enumerate() {
             if i == 0 {
-                ;
             } else if i % 27 == 0 {
                 s.push_str("\n\n");
             } else if i % 9 == 0 {
@@ -28,26 +28,26 @@ impl Sudoku {
     }
 }
 
+fn load_sudoku(filename: &str) -> Sudoku {
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+    let mut numbers: [u8; 81] = [0; 81];
+    for (i, c) in (0..81).zip(contents.split_whitespace()) {
+        numbers[i] = c.parse().unwrap()
+    }
+    return Sudoku { vals: numbers }
+}
+
 impl<'a> IntoIterator for &'a Sudoku {
    type Item = &'a u8;
    type IntoIter = std::slice::Iter<'a, u8>;
 
    fn into_iter(self) -> Self::IntoIter {
-       self.vals.into_iter()
+       self.vals.iter()
    }
 }
 
 fn main() {
-    let s = Sudoku {
-        vals: [1,2,3,4,5,6,7,8,9,
-               2,2,3,4,5,6,7,8,9,
-               3,2,3,4,5,6,7,8,9,
-               4,2,3,4,5,6,7,8,9,
-               5,2,3,4,5,6,7,8,9,
-               6,2,3,4,5,6,7,8,9,
-               7,2,3,4,5,6,7,8,9,
-               8,2,3,4,5,6,7,8,9,
-               9,2,3,4,5,6,7,8,9],
-    };
+    let s = load_sudoku("puzzle.txt");
     s.print();
 }

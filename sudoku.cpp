1e8dd2e79460accc8787e23a5dc0e749ac151909
Andrew Clough <andrew.clough@gmail.com>
2024-06-29 12:53:14 -0400

    Read in C++ sudoku

--- sudoku.cpp
+++ sudoku.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <bitset>
+#include <fstream>
 
 using namespace std;
 
@@ -19,12 +20,41 @@ private:
     Field blocks[9];
     Field rows[9];
     Field cols[9];
+
+    void set_value(int value) {
+        // FIXME
+    }
 public:
-    SuodkuProblem(ifstream inpput_file) {
+    SudokuProblem(ifstream& input_file) {
+        string value;
+        int i = 0;
+        while  (input_file >> value) {
+            try {
+                int spot_value = stoi(value);
+                set_value(spot_value);
+                i++;
+            } catch (const invalid_argument& e) {
+                // As long as we have 81 numbers, extraneous values aren't a problem
+            }
+        }
+        if (i != 81) {
+            cerr << "Got " << i << " numbers" << endl;
+            throw invalid_argument("Wrong number of numbers for a sudoku puzzle");
+            // Handle the error (e.g., throw an exception)
+        }
+
+        // Todo, check for more values
+        // Todo, close the file here or at the call site
     }
 };
 
 
 int main() {
+    ifstream input_file("puzzle.txt");
+    if (!input_file.is_open()) {
+        cout << "Error opening file" << endl;
+        return 1;
+    }
+    SudokuProblem problem(input_file);
     return 0;
 }

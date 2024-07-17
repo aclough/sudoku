624daf135016a4476f48e4dd3887c436ce9ae975
Andrew Clough <andrew.clough@gmail.com>
2024-07-17 19:42:47 -0400

    Use internal iteration in Rust too

--- test_speed.sh
+++ test_speed.sh
@@ -19,7 +19,7 @@ RUSTFLAGS="-C target-cpu=native" cargo build --release
 cp ./target/release/sudoku_rust sudoku_rust
 
 echo ""
-echo "C speed 10000 times"
+echo "C speed 10,000 times"
 time for i in {1..10000}; do ./sudoku_c hard-sudoku.txt > /dev/null; done
 
 echo ""
@@ -27,17 +27,17 @@ echo "C++ speed 1,000,000 times"
 time ./sudoku_cpp --iterations 1000000 hard-sudoku.txt > /dev/null
 
 echo ""
-echo "Nim speed 10000 times"
+echo "Nim speed 10,000 times"
 time for i in {1..10000}; do ./sudoku_nim hard-sudoku.txt > /dev/null; done
 
 echo ""
-echo "Rust speed 10000 times"
-time for i in {1..10000}; do ./sudoku_rust hard-sudoku.txt > /dev/null; done
+echo "Rust speed 1,000,000 times"
+time ./sudoku_rust hard-sudoku.txt 1000000 > /dev/null
 
 echo ""
-echo "Python speed 1000 times"
+echo "Python speed 1,000 times"
 time for i in {1..1000}; do python3 sudoku.py hard-sudoku.txt > /dev/null; done
 
 echo ""
-echo "Python calling c speed 1000 times"
+echo "Python calling c speed 1,000 times"
 time for i in {1..1000}; do python3 sudoku-with-c.py hard-sudoku.txt > /dev/null; done
